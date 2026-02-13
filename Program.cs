9741dc7d15e6ca4107d6ed7d9b11d1ab366c952a
bartrumjustin <bartrumjustin@gmail.com>
2026-02-12 20:57:47 -0500

    interface with 2 props and method,  class props and interface props with constructor and method. main uses param and default with method acting as display

--- Program.cs
+++ Program.cs
@@ -1,10 +1,55 @@
 ﻿namespace myInterface
 {
+    interface IStatePower
+    {
+        string State { get; set; }
+        string Fuel { get; set; }
+
+        string Display();
+    }
     internal class Program
     {
+        class PowerPlant : IStatePower
+        {
+            //interface
+            public string State { get; set; }
+            public string Fuel { get; set; }
+            //class prop
+            public int Power { get; set; }
+            public string Name { get; set; }
+            //default
+            public PowerPlant()
+            {
+                State = string.Empty;
+                Fuel = string.Empty;
+                Name = string.Empty;
+                Power = 0;
+            }
+            //param
+            public PowerPlant(string state, string fuel, int power, string name)
+            {
+                State = state;
+                Fuel = fuel;
+                Name = name;
+                Power = power;
+            }
+            public string Display()
+            {
+                return "Power Plant: " + Name + "\nLocation: " + State + "\nFuel Type: " + Fuel + "\nPower Output (MW): " + Power + "\n\n";
+
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            PowerPlant one = new PowerPlant("Michigan", "Nuclear", 800, "Palisades");
+            Console.WriteLine(one.Display());
+
+            PowerPlant two = new PowerPlant();
+            two.Name = "San Onofre";
+            two.Power = 2200;
+            two.State = "California";
+            two.Fuel = "Nuclear";
+            Console.WriteLine(two.Display());
         }
     }
 }
