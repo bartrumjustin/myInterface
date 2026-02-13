d5f1794983098a439f1d525fc5c72c3256ebe5b2
bartrumjustin <bartrumjustin@gmail.com>
2026-02-12 20:29:12 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace myInterface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
